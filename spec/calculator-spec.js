9e8386f3b402ca6cfa72f9fdf0936bad73957dd2
Sara Hamilton <sara@indeterminate.net>
2018-03-16 11:35:33 -0700

    add passing test for mercuryExpectancy

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -52,6 +52,6 @@ describe('Calculator', function() {
 
   it('will return difference between current age and life expectancy in Mercury years', function(){
     let result = reusableCalculator.mercuryExpectancy();
-    expect(result).toEqual(30);
+    expect(result).toEqual(204);
   })
 })
