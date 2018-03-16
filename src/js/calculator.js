1d63b2788b252683ddf973a28ce4e7ede6e97fb3
Sara Hamilton <sara@indeterminate.net>
2018-03-16 13:26:25 -0700

    remove 5 methods that are replaced by the planetExpectancy method

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -32,18 +32,6 @@ export class Calculator {
     return jupiterAge;
   }
 
-  compareToEarthExpectancy() {
-    const lifeExpectancy = 79;
-    let difference = lifeExpectancy - this.earthAge;
-    if (difference > 0) {
-      return `Your age is ${difference} years less than the average life expectancy in Earth years.`;
-    } else if (difference === 0) {
-      return "Your age is at the average life expectancy in Earth years."
-    } else {
-      return `Your age is ${-difference} years past the average life expectancy in Earth years.`;
-    }
-  }
-
   planetExpectancy(planet) {
     const lifeExpectancy = 79;
     let factor = 0;
@@ -73,54 +61,6 @@ export class Calculator {
       return `Your age is at the average life expectancy in ${planet} years.`
     } else {
       return `Your age is ${-difference} years past the average life expectancy in ${planet} years.`;
-    }
-  }
-
-  mercuryExpectancy() {
-    const lifeExpectancy = 79;
-    let difference = Math.floor((lifeExpectancy - this.earthAge)/.24);
-    if (difference > 0) {
-      return `Your age is ${difference} years less than the average life expectancy in Mercury years.`;
-    } else if (difference === 0) {
-      return "Your age is at the average life expectancy in Mercury years."
-    } else {
-      return `Your age is ${-difference} years past the average life expectancy in Mercury years.`;
-    }
-  }
-
-  venusExpectancy() {
-    const lifeExpectancy = 79;
-    let difference = Math.floor((lifeExpectancy - this.earthAge)/.62);
-    if (difference > 0) {
-      return `Your age is ${difference} years less than the average life expectancy in Venus years.`;
-    } else if (difference === 0) {
-      return "Your age is at the average life expectancy in Venus years."
-    } else {
-      return `Your age is ${-difference} years past the average life expectancy in Venus years.`;
-    }
-  }
-
-  marsExpectancy() {
-    const lifeExpectancy = 79;
-    let difference = Math.floor((lifeExpectancy - this.earthAge)/1.88);
-    if (difference > 0) {
-      return `Your age is ${difference} years less than the average life expectancy in Mars years.`;
-    } else if (difference === 0) {
-      return "Your age is at the average life expectancy in Mars years."
-    } else {
-      return `Your age is ${-difference} years past the average life expectancy in Mars years.`;
-    }
-  }
-
-  jupiterExpectancy() {
-    const lifeExpectancy = 79;
-    let difference = Math.floor((lifeExpectancy - this.earthAge)/11.86);
-    if (difference > 0) {
-      return `Your age is ${difference} years less than the average life expectancy in Jupiter years.`;
-    } else if (difference === 0) {
-      return "Your age is at the average life expectancy in Jupiter years."
-    } else {
-      return `Your age is ${-difference} years past the average life expectancy in Jupiter years.`;
     }
   }
 
