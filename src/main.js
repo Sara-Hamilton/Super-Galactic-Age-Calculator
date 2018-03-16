3b54b987d7df0968fa91e9074d11bb85f54e260a
Sara Hamilton <sara@indeterminate.net>
2018-03-16 16:19:08 -0700

    add main.js and index.html

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { Calculator } from './js/calculator.js';
+import './styles.css';
+
+$(document).ready(function(){
+  $('#calculatorForm').submit(function(event) {
+    event.preventDefault();
+    let birthday = $('#birthday').val();
+    let age = ((Date.now() - Date.parse(birthday)))/31536000000;
+
+    let galacticCalculator = new Calculator(age);
+
+    let planets = ["Earth", "Mercury", "Venus", "Mars", "Jupiter"];
+
+    planets.forEach(function(planet) {
+      let planetAge = galacticCalculator.ageOnPlanet(planet);
+      $("#" + planet + "Age").text(planetAge);
+    });
+
+    planets.forEach(function(planet) {
+      let planetExpectancy = galacticCalculator.planetExpectancy(planet);
+      $("#" + planet + "Expectancy").text(planetExpectancy);
+    });
+
+    $("#results").show();
+
+
+  });
+});
