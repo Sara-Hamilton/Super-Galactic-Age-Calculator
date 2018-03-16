8a03e6c3e47f73104313d6cec8747c7a44fd7a97
Sara Hamilton <sara@indeterminate.net>
2018-03-16 10:28:17 -0700

    add secondsBetweenDates method and failing test

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -5,10 +5,17 @@ describe('Calculator', function() {
 
   beforeEach(function() {
     reusableCalculator = new Calculator(30);
-  });
+  })
 
   it('will convert years to seconds', function() {
     let result = reusableCalculator.convertToSeconds();
     expect(result).toEqual(946080000);
   })
-});
+
+  it('will return the difference in seconds between two dates', function(){
+    let date1 = new Date('April 1, 2010');
+    let date2 = new Date('April 1, 2011');
+    let result = reusableCalculator.secondsBetweenDates(date1, date2);
+    expect(result).toEqual(1);
+  })
+})
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,4 +6,9 @@ export class Calculator {
   convertToSeconds() {
     return (this.earthAge * 31536000);
   }
+
+  secondsBetweenDates(date1, date2) {
+    let difference = Math.abs(date1 - date2)/1000;
+    return difference;
+  }
 }
