82507251b328c26d0bedfe665b17eb60b991d54a
Sara Hamilton <sara@indeterminate.net>
2018-03-16 14:22:15 -0700

    replace repeated switch statement with method planetFactor

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -12,7 +12,7 @@ export class Calculator {
     return difference;
   }
 
-  ageOnPlanet(planet) {
+  planetFactor(planet) {
     let factor = 0;
     switch (planet) {
       case 'Mercury':
@@ -30,32 +30,55 @@ export class Calculator {
       default:
         factor = 1;
     }
+    return factor;
+  }
+
+  ageOnPlanet(planet) {
+    // let factor = 0;
+    // switch (planet) {
+    //   case 'Mercury':
+    //     factor = .24;
+    //     break;
+    //   case 'Venus':
+    //     factor = .62;
+    //     break;
+    //   case 'Mars':
+    //     factor = 1.88;
+    //     break;
+    //   case 'Jupiter':
+    //     factor = 11.86;
+    //     break;
+    //   default:
+    //     factor = 1;
+    // }
+    let factor = this.planetFactor(planet);
     let planetAge = Math.floor(this.earthAge/factor);
     return planetAge;
   }
 
   planetExpectancy(planet) {
     const lifeExpectancy = 79;
-    let factor = 0;
-    switch (planet) {
-      case 'Earth':
-        factor = 1;
-        break;
-      case 'Mercury':
-        factor = .24;
-        break;
-      case 'Venus':
-        factor = .62;
-        break;
-      case 'Mars':
-        factor = 1.88;
-        break;
-      case 'Jupiter':
-        factor = 11.86;
-        break;
-      default:
-        factor = 1;
-    }
+    // let factor = 0;
+    // switch (planet) {
+    //   case 'Earth':
+    //     factor = 1;
+    //     break;
+    //   case 'Mercury':
+    //     factor = .24;
+    //     break;
+    //   case 'Venus':
+    //     factor = .62;
+    //     break;
+    //   case 'Mars':
+    //     factor = 1.88;
+    //     break;
+    //   case 'Jupiter':
+    //     factor = 11.86;
+    //     break;
+    //   default:
+    //     factor = 1;
+    // }
+    let factor = this.planetFactor(planet);
     let difference = Math.floor((lifeExpectancy - this.earthAge)/factor);
     if (difference > 0) {
       return `Your age is ${difference} years less than the average life expectancy in ${planet} years.`;
